4a3aaf63d1d48b76d994948fabda15ee9fd233ae
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-05-05 01:13:11 +0500

    session provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SessionProvider from "../components/SessionProvider";
 import SideBar from "../components/SideBar";
 import "../styles/globals.css";
 
@@ -10,15 +11,18 @@ export default function RootLayout({
     <html>
       <head />
       <body>
-        <div className="flex">
+        <SessionProvider>
           {" "}
-          {/* Sidebar */}
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem] ">
-            <SideBar />
+          <div className="flex">
+            {" "}
+            {/* Sidebar */}
+            <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem] ">
+              <SideBar />
+            </div>
+            {/* ClientProvider- Notifications */}
+            <div className="bg-[#343541] flex-1"> {children}</div>
           </div>
-          {/* ClientProvider- Notifications */}
-          <div className="bg-[#343541] flex-1"> {children}</div>
-        </div>
+        </SessionProvider>
       </body>
     </html>
   );
